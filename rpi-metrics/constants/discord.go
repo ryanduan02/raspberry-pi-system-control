b86a349b3f8f6d3c283786d20c6d9ba7cd189991
Ryan Duan <ryanduan02@gmail.com>
2026-01-08 20:18:05 -0500

    fix message separater

--- rpi-metrics/constants/discord.go
+++ rpi-metrics/constants/discord.go
@@ -2,5 +2,5 @@ package constants
 
 const (
 	// DiscordMessageSeparator is prefixed to every Discord message sent by the bot.
-	DiscordMessageSeparator = "________________________________"
+	DiscordMessageSeparator = "--------------------------------"
 )
